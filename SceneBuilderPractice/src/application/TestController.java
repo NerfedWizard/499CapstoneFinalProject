a42e7d72dd3a1739efd8d669d613aad916c690f1
NerfedWizard <loelnelson@gmail.com>
2021-02-18 03:19:46 -0600

    Drop Down Menu Working with action events

--- SceneBuilderPractice/src/application/TestController.java
+++ SceneBuilderPractice/src/application/TestController.java
@@ -3,15 +3,20 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class TestController implements Initializable {
 	@FXML
@@ -38,12 +43,22 @@ public class TestController implements Initializable {
 	private RadioButton forgotPasswordRadioButton;
 	@FXML
 	private Label wrongTryLabel;
+	@FXML
+	private MenuButton userDropDownMenuButton;
+	@FXML
+	private MenuItem teacherMenuItem;
+	@FXML
+	private MenuItem studentMenuItem;
+	@FXML
+	private MenuItem guardianMenuItem;
 	private int failedCounter = 0;
+	private String userSelection = "";
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 //		loginLabel.setText("User Login");
 		textShowLabel.setText("Enter Information");
+		
 //		loginButton.isPressed();
 //		if (forgotPasswordUsernameHyperLink.isHover()) {
 //			textShowLabel.setText("Sent to email");
@@ -51,42 +66,99 @@ public class TestController implements Initializable {
 //		if (forgotPasswordUsernameHyperLink.isPressed()) {
 //			forgotInfoHyperText();
 //		}
+//		userDropDownMenuButton.setContentDisplay("Teacher");
+//		EventHandler<ActionEvent> event1 = new EventHandler<ActionEvent>() { 
+//            public void handle(ActionEvent e) 
+//            { 
+//            	((MenuItem)e.getSource()).setText("selected"); 
+//            	userSelection = ((MenuItem)e.getSource()).getText();
+//            	System.out.println(userSelection);
+//            } 
+//        }; 
+//	}
+//
+//	public void login() {
+//		final String userSelection = "";
+		EventHandler<ActionEvent> event1 = new EventHandler<ActionEvent>() { 
+            public void handle(ActionEvent e) 
+            { 
+            	userSelection = ((MenuItem)e.getSource()).getText();
+//            	((MenuItem)e.getSource()).setText("selected"); 
+            	
+//            	System.out.println(userSelection);
+            } 
+        }; 
+//        EventHandler<MouseEvent> loginEvent = new EventHandler<MouseEvent>() {
+//
+//			@Override
+//			public void handle(MouseEvent e) {
+//				if (userSelection.equals("Student")) {
+//					if (usernameTextField.getText().equals("Loel")) {
+//						if (passwordTextField.getText().equals("123")) {
+//							textShowLabel.setText("Success!");
+//						}
+//					}
+//				}
+//				
+//			}
+//        	
+//        };
+        studentMenuItem.setOnAction(event1);
+        guardianMenuItem.setOnAction(event1);
+        teacherMenuItem.setOnAction(event1);
+        
+//        if(loginButton.isPressed()) {
 	}
-
-	public void login() {
-		if (studentCheckBox.isSelected()) {
+        public void userLogin() {
+        	if (userSelection.equals("Student")) {
+				if (usernameTextField.getText().equals("Loel")) {
+					if (passwordTextField.getText().equals("123")) {
+						textShowLabel.setText("Success!");
+					}else {
+						textShowLabel.setText("Invalid Password");
+					}
+				}else {
+					textShowLabel.setText("Invalid UserName");
+				}
+			}
+        
+//        loginButton.setOnMouseClicked(loginEvent);
+        
+//		userDropDownMenuButton.getItems();
+//		System.out.println(userSelection);
+//		if (userSelection.equals("2")) {
 //			String username = "";
 //			String password = "";
 ////			username = usernameTextField.getText();
 //			password = passwordTextField.getText();
 
-			if (usernameTextField.getText().equals("Loel")) {
-				if (passwordTextField.getText().equals("123")) {
-					textShowLabel.setText("Successful Login");
-
-				}
-			} else if (!usernameTextField.getText().equals("Loel")) {
-				failedCounter += 1;
-			}
-			if (failedCounter > 3) {
-				wrongTryLabel.setText("Select Button To Reset Username/Password");
-			}
-		}
-
-		if (parentCheckBox.isSelected()) {
-			String username = "";
-			String password = "";
-
-			username = usernameTextField.getText().toString();
-			password = passwordTextField.getText();
-
-			if ("Loel" == username) {
-				if ("123" == password) {
-					textShowLabel.setText("Successful Login");
-
-				}
-			}
-		}
+//			if (usernameTextField.getText().equals("Loel")) {
+//				if (passwordTextField.getText().equals("123")) {
+//					textShowLabel.setText("Successful Login");
+//
+//				}
+//			} else if (!usernameTextField.getText().equals("Loel")) {
+//				failedCounter += 1;
+//			}
+//			if (failedCounter > 3) {
+//				wrongTryLabel.setText("Select Button To Reset Username/Password");
+//			}
+//		}
+//
+//		if (parentCheckBox.isSelected()) {
+//			String username = "";
+//			String password = "";
+//
+//			username = usernameTextField.getText().toString();
+//			password = passwordTextField.getText();
+//
+//			if ("Loel" == username) {
+//				if ("123" == password) {
+//					textShowLabel.setText("Successful Login");
+//
+//				}
+//			}
+//		}
 //		if (forgotPasswordRadioButton.isSelected()) {
 //			
 //		}
